c2d029245c66cdd449f996d93839791f903fb8b8
Devolder, Eric <eric.devolder@mastercard.com>
2018-07-05 16:41:47 +0200

    release 0.3.0 - now using real slot index. Choosing an empty slot will abort the flow.

--- src/p11perftest.cpp
+++ src/p11perftest.cpp
@@ -80,7 +80,7 @@ int main(int argc, char **argv)
 	      << std::to_string( info.cryptokiVersion.minor ) << std::endl ;
 	      
     // only slots with connected token
-    std::vector<p11::SlotId> slots = p11::Slot::get_available_slots( module, true );
+    std::vector<p11::SlotId> slots = p11::Slot::get_available_slots( module, false );
 
     p11::Slot slot( module, slots.at( argslot ) );
 
@@ -90,92 +90,96 @@ int main(int argc, char **argv)
 	      << std::to_string( slot_info.firmwareVersion.major ) << "."
 	      << std::to_string( slot_info.firmwareVersion.minor ) << std::endl;
 
-    // print firmware version of the token
-    p11::TokenInfo token_info = slot.get_token_info();
-    std::cout << "Token firmware version: "
-	      << std::to_string( token_info.firmwareVersion.major ) << "."
-	      << std::to_string( token_info.firmwareVersion.minor ) << std::endl;
+    // detect if we have a token inserted
+    if(slot_info.flags & CKF_TOKEN_PRESENT) {
+	// print firmware version of the token
+	p11::TokenInfo token_info = slot.get_token_info();
+	std::cout << "Token firmware version: "
+		  << std::to_string( token_info.firmwareVersion.major ) << "."
+		  << std::to_string( token_info.firmwareVersion.minor ) << std::endl;
 
-    auto session = p11::Session(slot, false);
+	auto session = p11::Session(slot, false);
 
-    std::string argpwd = vm["password"].as<std::string>();
-    p11::secure_string pwd( argpwd.data(), argpwd.data()+argpwd.length() );
-    session.login(p11::UserType::User, pwd );
+	std::string argpwd = vm["password"].as<std::string>();
+	p11::secure_string pwd( argpwd.data(), argpwd.data()+argpwd.length() );
+	session.login(p11::UserType::User, pwd );
 
-    std::string test1 { "12345678" };
-    std::vector<uint8_t> testvec1(test1.data(),test1.data()+test1.length());
-    std::string test2 { "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh" };
-    std::vector<uint8_t> testvec2(test2.data(),test2.data()+test2.length());
+	std::string test1 { "12345678" };
+	std::vector<uint8_t> testvec1(test1.data(),test1.data()+test1.length());
+	std::string test2 { "12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh12345678abcdefgh" };
+	std::vector<uint8_t> testvec2(test2.data(),test2.data()+test2.length());
 
-    // needed for AES
-    std::string test3 { "0123456789ABCDEF" };
-    std::vector<uint8_t> testvec3(test3.data(),test3.data()+test3.length());
+	// needed for AES
+	std::string test3 { "0123456789ABCDEF" };
+	std::vector<uint8_t> testvec3(test3.data(),test3.data()+test3.length());
 
-    // big vectors
-    std::vector<uint8_t> testvec4(800, 64);
-    std::vector<uint8_t> testvec5(8000, 65);
+	// big vectors
+	std::vector<uint8_t> testvec4(800, 64);
+	std::vector<uint8_t> testvec5(8000, 65);
 
-    P11RSASigBenchmark rsa1( session, "rsa-1");
-    rsa1.execute(testvec1, argiter);
-    rsa1.execute(testvec2, argiter);
-    rsa1.execute(testvec4, argiter);
-    rsa1.execute(testvec5, argiter);
+	P11RSASigBenchmark rsa1( session, "rsa-1");
+	rsa1.execute(testvec1, argiter);
+	rsa1.execute(testvec2, argiter);
+	rsa1.execute(testvec4, argiter);
+	rsa1.execute(testvec5, argiter);
 
-    P11RSASigBenchmark rsa2( session, "rsa-2");
-    rsa2.execute(testvec1, argiter);
-    rsa2.execute(testvec2, argiter);
-    rsa2.execute(testvec4, argiter);
-    rsa2.execute(testvec5, argiter);
+	P11RSASigBenchmark rsa2( session, "rsa-2");
+	rsa2.execute(testvec1, argiter);
+	rsa2.execute(testvec2, argiter);
+	rsa2.execute(testvec4, argiter);
+	rsa2.execute(testvec5, argiter);
 
-    P11DES3ECBBenchmark des1( session, "des-1");
-    des1.execute(testvec1, argiter);
-    des1.execute(testvec2, argiter);
-    des1.execute(testvec4, argiter);
-    des1.execute(testvec5, argiter);
+	P11DES3ECBBenchmark des1( session, "des-1");
+	des1.execute(testvec1, argiter);
+	des1.execute(testvec2, argiter);
+	des1.execute(testvec4, argiter);
+	des1.execute(testvec5, argiter);
 
-    P11DES3ECBBenchmark des2( session, "des-2");
-    des2.execute(testvec1, argiter);
-    des2.execute(testvec2, argiter);
-    des2.execute(testvec4, argiter);
-    des2.execute(testvec5, argiter);
+	P11DES3ECBBenchmark des2( session, "des-2");
+	des2.execute(testvec1, argiter);
+	des2.execute(testvec2, argiter);
+	des2.execute(testvec4, argiter);
+	des2.execute(testvec5, argiter);
 
-    P11DES3CBCBenchmark descbc1( session, "des-1");
-    descbc1.execute(testvec1, argiter);
-    descbc1.execute(testvec2, argiter);
-    descbc1.execute(testvec4, argiter);
-    descbc1.execute(testvec5, argiter);
+	P11DES3CBCBenchmark descbc1( session, "des-1");
+	descbc1.execute(testvec1, argiter);
+	descbc1.execute(testvec2, argiter);
+	descbc1.execute(testvec4, argiter);
+	descbc1.execute(testvec5, argiter);
 
-    P11DES3CBCBenchmark descbc2( session, "des-2");
-    descbc2.execute(testvec1, argiter);
-    descbc2.execute(testvec2, argiter);
-    descbc2.execute(testvec4, argiter);
-    descbc2.execute(testvec5, argiter);
+	P11DES3CBCBenchmark descbc2( session, "des-2");
+	descbc2.execute(testvec1, argiter);
+	descbc2.execute(testvec2, argiter);
+	descbc2.execute(testvec4, argiter);
+	descbc2.execute(testvec5, argiter);
 
-    P11AESECBBenchmark aes1( session, "aes-1");
-    aes1.execute(testvec3, argiter);
-    aes1.execute(testvec2, argiter);
-    aes1.execute(testvec4, argiter);
-    aes1.execute(testvec5, argiter);
+	P11AESECBBenchmark aes1( session, "aes-1");
+	aes1.execute(testvec3, argiter);
+	aes1.execute(testvec2, argiter);
+	aes1.execute(testvec4, argiter);
+	aes1.execute(testvec5, argiter);
 
-    P11AESECBBenchmark aes2( session, "aes-2");
-    aes2.execute(testvec3, argiter);
-    aes2.execute(testvec2, argiter);
-    aes2.execute(testvec4, argiter);
-    aes2.execute(testvec5, argiter);
+	P11AESECBBenchmark aes2( session, "aes-2");
+	aes2.execute(testvec3, argiter);
+	aes2.execute(testvec2, argiter);
+	aes2.execute(testvec4, argiter);
+	aes2.execute(testvec5, argiter);
 
-    P11AESCBCBenchmark aes1cbc( session, "aes-1");
-    aes1cbc.execute(testvec3, argiter);
-    aes1cbc.execute(testvec2, argiter);
-    aes1cbc.execute(testvec4, argiter);
-    aes1cbc.execute(testvec5, argiter);
+	P11AESCBCBenchmark aes1cbc( session, "aes-1");
+	aes1cbc.execute(testvec3, argiter);
+	aes1cbc.execute(testvec2, argiter);
+	aes1cbc.execute(testvec4, argiter);
+	aes1cbc.execute(testvec5, argiter);
 
-    P11AESCBCBenchmark aes2cbc( session, "aes-2");
-    aes2cbc.execute(testvec3, argiter);
-    aes2cbc.execute(testvec2, argiter);
-    aes2cbc.execute(testvec4, argiter);
-    aes2cbc.execute(testvec5, argiter);
+	P11AESCBCBenchmark aes2cbc( session, "aes-2");
+	aes2cbc.execute(testvec3, argiter);
+	aes2cbc.execute(testvec2, argiter);
+	aes2cbc.execute(testvec4, argiter);
+	aes2cbc.execute(testvec5, argiter);
 
-    session.logoff();
-
+	session.logoff();
+    } else {
+      std::cout << "The slot at index " << argslot << " has no token. Aborted." << std::endl;
+    }
     return 0;
 }
