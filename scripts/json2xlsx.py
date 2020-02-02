7042e31b4bcb61a4dd5189668f891c9b2e6f9cf9
Eric Devolder <eric.devolder@mastercard.com>
2020-02-02 16:44:32 -0600

    adapted python script "json2xlsx.py" to become independent of future format changes

--- scripts/json2xlsx.py
+++ scripts/json2xlsx.py
@@ -8,47 +8,13 @@ import argparse
 noop = lambda x : x
 
 cast = {
-    'label' : noop,
-    'vector_size': int,
-    'vector_unit': noop,
-    'algorithm' : noop,
-    'threads' : int,
     'iterations' : int,
-    'totalcount' : int,
-    'errorcode' : noop,
-    'wallclock' : float,
-    'timer_resolution' : float,
-    'timer_unit' : noop,
-    'timer_error' : float,
-    'timer_relerr' : float,
-    'latency_average_value' : float,
-    'latency_average_unit' : noop,
-    'latency_average_error' : float,
-    'latency_average_relerr' : float,
-    'latency_maximum_value' : float,
-    'latency_maximum_unit' : noop,
-    'latency_maximum_error' : float,
-    'latency_maximum_relerr' : float,
-    'latency_minimum_value' : float,
-    'latency_minimum_unit' : noop,
-    'latency_minimum_error' : float,
-    'latency_minimum_relerr' : float,
-    'tps_thread_value' : float,
-    'tps_thread_unit' : noop,
-    'tps_thread_error' : float,
-    'tps_thread_relerr' : float,
-    'tps_global_value' : float,
-    'tps_global_unit' : noop,
-    'tps_global_error' : float,
-    'tps_global_relerr' : float,
-    'throughput_thread_value' : float,
-    'throughput_thread_unit' : noop,
-    'throughput_thread_error' : float,
-    'throughput_thread_relerr' : float,
-    'throughput_global_value' : float,
-    'throughput_global_unit' : noop,
-    'throughput_global_error' : float,
-    'throughput_global_relerr' : float,
+    'total iterations' : int,
+    'threads' : int,
+    'size' : int,
+    'value' : float,
+    'error' : float,
+    'relerr': float
 }
 
 
@@ -68,7 +34,7 @@ class Converter:
         self.toxlsx = toxlsx
         self.row = 0
         self.col = 0
-        self.title = None
+        self.titlerow = False
 
     def __enter__(self):
         self.workbook = xlsxwriter.Workbook(self.toxlsx, options={'nan_inf_to_errors': True})
@@ -79,54 +45,38 @@ class Converter:
         self.workbook.close()
 
     def add_a_row(self, filename, testcase, key, vectorname, vector):
-        if self.title is None:  # the title has not been defined yet, let's fetch all the info from the file itself
-            self.title = list(vector.keys())
-            self.title.insert(self.row, 'vector name')
-            self.title.insert(self.row, 'label')
-            self.title.insert(self.row, 'testcase')
-            self.title.insert(self.row, 'filename')
 
-            for item in self.title:
-                # for resolution and vector, we have one level only
-                if item in ('timer', 'vector') :
-                    for sub1k,sub1v in vector[item].items():
-                        self.worksheet.write(self.row, self.col, f"{item} {sub1k}")
-                        self.col+=1
-
-                # for latency, tps and throughput we have two levels down to explore.
-                elif item in ('latency', 'tps', 'throughput') :
-                    for sub1k,sub1v in vector[item].items():
-                        for sub2k,sub2v in sub1v.items():
-                            self.worksheet.write(self.row, self.col, f"{item} {sub1k} {sub2k}")
-                            self.col+=1
+        def recursive_title(vector, prefix):
+            for subk,subv in vector.items():
+                if not isinstance(subv,(dict)):
+                    self.worksheet.write(self.row, self.col, (prefix + f"{subk} ").strip())
+                    self.col+=1
                 else:
-                    self.worksheet.write(self.row,self.col,item)
-                    self.col+=1
-            self.row+=1
-            self.col=0
+                    recursive_title(subv, prefix + f"{subk} ")
 
-        # insert data
-        self.worksheet.write(self.row,0,filename)
-        self.worksheet.write(self.row,1,testcase)
-        self.worksheet.write(self.row,2,key)
-        self.worksheet.write(self.row,3,vectorname)
-        self.col=4
-        for prop,val in vector.items():
-            # for resolution, we have one level only
-            if prop in ('timer', 'vector'):
-                for sub1k,sub1v in val.items():
-                    self.worksheet.write(self.row,self.col,cast[f"{prop}_{sub1k}"](sub1v))
+        def recursive_value(vector, prefix):
+            for subk,subv in vector.items():
+                if not isinstance(subv,(dict)):
+                    self.worksheet.write(self.row, self.col, cast.get(subk, noop)(subv))
                     self.col+=1
-            # for latency, tps and throughput we have two levels down to explore.
-            elif prop in ('latency', 'tps', 'throughput'):
-                for sub1k,sub1v in val.items():
-                    for sub2k,sub2v in sub1v.items():
-                        self.worksheet.write(self.row,self.col,cast[f"{prop}_{sub1k}_{sub2k}"](sub2v))
-                        self.col+=1
-            else:
-                self.worksheet.write(self.row,self.col,cast[prop](val))
-                self.col+=1
-            # advance in table
+                else:
+                    recursive_value(subv, prefix + f"{subk} ")
+
+        # the title has not been defined yet, let's fetch all the info from the file itself
+        if self.titlerow is False:
+            self.worksheet.write(self.row,0,'vector name')
+            self.worksheet.write(self.row,1,'label')
+            self.worksheet.write(self.row,2,'test case')
+            self.worksheet.write(self.row,3,'file name')
+
+            recursive_title(vector, "")
+
+            self.titlerow = True
+
+        else:
+            # we have defined the title already, proceed with row data
+            recursive_value(vector, "")
+
         self.row+=1
         self.col=0
 
