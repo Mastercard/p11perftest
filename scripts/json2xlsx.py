a16bfe33334b37690139605375ea82f5a7941fa5
Eric Devolder <eric.devolder@mastercard.com>
2020-02-05 11:23:31 -0600

    Spreadsheets are now formatted, support for PyInstaller added

--- scripts/json2xlsx.py
+++ scripts/json2xlsx.py
@@ -21,13 +21,16 @@ cast = {
 def retrieve_rows(listofjsons):
     for f in listofjsons:
         # recover JSON structure
-        testcases = json.loads( f.read() );
+        try:
+            testcases = json.loads( f.read() );
 
-        for testcase,keys in testcases.items():
-            for key, vectors in keys.items():
-                for vectorname, vector in vectors.items():
-                    yield f.name,testcase,key,vectorname,vector
+            for testcase,keys in testcases.items():
+                for key, vectors in keys.items():
+                    for vectorname, vector in vectors.items():
+                        yield f.name,testcase,key,vectorname,vector
 
+        except Exception as e:
+            print(f"*** got an error while processing {f.name}: \"{e}\", skipping that file")
 
 class Converter:
     def __init__(self, toxlsx):
@@ -35,26 +38,45 @@ class Converter:
         self.row = 0
         self.col = 0
         self.titlerow = False
+        self.maxrow = self.row
+        self.maxcol = self.col
+        self.columns = list()
 
     def __enter__(self):
         self.workbook = xlsxwriter.Workbook(self.toxlsx, options={'nan_inf_to_errors': True})
         self.worksheet = self.workbook.add_worksheet()
+        self.percent_format = self.workbook.add_format({'num_format' : '0.00%' })
+
         return self
 
     def __exit__(self ,type, value, traceback):
+        self.worksheet.add_table(*self.size, {'style': 'Table Style Medium 16', 'columns' : self.columns})
         self.workbook.close()
+
+    @property
+    def size(self):
+        return 0,0,self.maxrow-1,self.maxcol-1
 
     def add_a_row(self, filename, testcase, key, vectorname, vector):
 
-        def recursive_title(vector, prefix):
+        def recursive_title(vector, prefix=""):
             for subk,subv in vector.items():
                 if not isinstance(subv,(dict)):
-                    self.worksheet.write(self.row, self.col, (prefix + f"{subk} ").strip())
+                    column_title = (prefix + f"{subk} ").strip()
+                    column_dict = { 'header':column_title }
+                    if column_title.endswith('relerr'): # special case: if relerr in the name, then we show percents
+                        column_dict['format'] = self.percent_format
+
+                    # remember for later, when we call __exit__()
+                    self.columns.append(column_dict)
+
+                    # write cell
+                    self.worksheet.write(self.row, self.col, column_title)
                     self.col+=1
                 else:
                     recursive_title(subv, prefix + f"{subk} ")
 
-        def recursive_value(vector, prefix):
+        def recursive_value(vector, prefix=""):
             for subk,subv in vector.items():
                 if not isinstance(subv,(dict)):
                     self.worksheet.write(self.row, self.col, cast.get(subk, noop)(subv))
@@ -64,28 +86,35 @@ class Converter:
 
         # the title has not been defined yet, let's fetch all the info from the file itself
         if self.titlerow is False:
-            self.worksheet.write(self.row,3,'vector name')
-            self.worksheet.write(self.row,2,'label')
-            self.worksheet.write(self.row,1,'test case')
             self.worksheet.write(self.row,0,'file name')
+            self.worksheet.write(self.row,1,'test case')
+            self.worksheet.write(self.row,2,'key label')
+            self.worksheet.write(self.row,3,'vector name')
+            self.columns.extend( [
+                { 'header' : 'file name'},
+                { 'header' : 'test case'},
+                { 'header' : 'key label'},
+                { 'header' : 'vector name'},])
+
             self.col=4
 
-            recursive_title(vector, "")
+            recursive_title(vector)
+            self.maxcol = self.col
 
             self.titlerow = True
+            self.row+=1
 
-        else:
-            self.worksheet.write(self.row,3,vectorname)
-            self.worksheet.write(self.row,2,key)
-            self.worksheet.write(self.row,1,testcase)
-            self.worksheet.write(self.row,0,filename)
-            self.col=4
-            # we have defined the title already, proceed with row data
-            recursive_value(vector, "")
+        self.worksheet.write(self.row,0,filename)
+        self.worksheet.write(self.row,1,testcase)
+        self.worksheet.write(self.row,2,key)
+        self.worksheet.write(self.row,3,vectorname)
+        self.col=4
+        # we have defined the title already, proceed with row data
+        recursive_value(vector)
 
         self.row+=1
         self.col=0
-
+        self.maxrow = self.row
 
 if __name__ == '__main__':
 
