290c2580cfb0904cc32af9752d4b526eb815e780
Marcel Armour <marcel.armour@mastercard.com>
2022-11-29 14:00:02 +0000

    add switch to remove error regions

--- scripts/gengraphs.py
+++ scripts/gengraphs.py
@@ -122,15 +122,17 @@ def generate_graphs(xlsfp, sheetname, xlsfp2):
                 fig, (ax, ax2) = plt.subplots(2, figsize=(16, 16), height_ratios=(3, 1))
 
                 ax.plot(frame1[xvar], frame1[f'{measure} global value'], marker='v', color='tab:blue')
-                ax.plot(frame1[xvar], frame1['tp_upper'], color='tab:blue', alpha=0.4)
-                ax.plot(frame1[xvar], frame1['tp_lower'], color='tab:blue', alpha=0.4)
-                ax.fill_between(frame1[xvar], frame1['tp_upper'], frame1['tp_lower'], facecolor='tab:blue', alpha=0.4)
+                if not args.error:
+                    ax.plot(frame1[xvar], frame1['tp_upper'], color='tab:blue', alpha=0.4)
+                    ax.plot(frame1[xvar], frame1['tp_lower'], color='tab:blue', alpha=0.4)
+                    ax.fill_between(frame1[xvar], frame1['tp_upper'], frame1['tp_lower'], facecolor='tab:blue', alpha=0.4)
                 if args.comparison:
                     ax.plot(frame2[xvar], frame2[f'{measure} global value'], marker='^', color='tab:blue', linestyle='--')
-                    ax.plot(frame2[xvar], frame2['tp_upper'], color='tab:blue', alpha=0.4, linestyle='--')
-                    ax.plot(frame2[xvar], frame2['tp_lower'], color='tab:blue', alpha=0.4, linestyle='--')
-                    ax.fill_between(frame2[xvar], frame2['tp_upper'], frame2['tp_lower'],
-                                    facecolor='tab:blue', alpha=0.4, linestyle='--')
+                    if not args.error:
+                        ax.plot(frame2[xvar], frame2['tp_upper'], color='tab:blue', alpha=0.4, linestyle='--')
+                        ax.plot(frame2[xvar], frame2['tp_lower'], color='tab:blue', alpha=0.4, linestyle='--')
+                        ax.fill_between(frame2[xvar], frame2['tp_upper'], frame2['tp_lower'],
+                                        facecolor='tab:blue', alpha=0.4, linestyle='--')
 
                 title = format_title(testcase, item)
                 if args.comparison:
@@ -144,39 +146,43 @@ def generate_graphs(xlsfp, sheetname, xlsfp2):
 
                 ax1 = ax.twinx() # add second plot to the same axes, sharing x-axis
                 ax1.plot(np.nan, marker='v', label=f'{measure}, global {xlsfp.label[1]}', color='tab:blue')  # Make an agent in ax
-                ax1.plot(np.nan, label=f'{measure} error', color='tab:blue', alpha=0.4)  # Make an agent in ax
                 if args.comparison:
                     ax1.plot(np.nan, marker='^', label=f'{measure}, global {xlsfp2.label[1]}', color='tab:blue', linestyle='--')  # Make an agent in ax
 
                 ax1.plot(frame1[xvar], frame1['latency average value'], label=f'latency {xlsfp.label[1]}', color='black', marker='p')
-                ax1.plot(frame1[xvar], frame1['latency_upper'], label='latency error region', color='grey', alpha=0.4)
-                ax1.plot(frame1[xvar], frame1['latency_lower'], color='grey', alpha=0.4)
-                ax1.fill_between(frame1[xvar], frame1['latency_upper'], frame1['latency_lower'],
-                                 facecolor='grey', alpha=0.4)
+                if not args.error:
+                    ax1.plot(np.nan, label=f'{measure} error', color='tab:blue', alpha=0.4)  # Make an agent in ax
+                    ax1.plot(frame1[xvar], frame1['latency_upper'], label='latency error region', color='grey', alpha=0.4)
+                    ax1.plot(frame1[xvar], frame1['latency_lower'], color='grey', alpha=0.4)
+                    ax1.fill_between(frame1[xvar], frame1['latency_upper'], frame1['latency_lower'],
+                                     facecolor='grey', alpha=0.4)
 
                 if args.comparison:
                     ax1.plot(frame2[xvar], frame2['latency average value'], label=f'latency {xlsfp2.label[1]}', color='black', marker='*', linestyle='--')
-                    ax1.plot(frame2[xvar], frame2['latency_upper'], color='grey', alpha=0.4, linestyle='--')
-                    ax1.plot(frame2[xvar], frame2['latency_lower'], color='grey', alpha=0.4, linestyle='--')
-                    ax1.fill_between(frame2[xvar], frame2['latency_upper'], frame2['latency_lower'],
-                                     facecolor='grey', alpha=0.4, linestyle='--')
+                    if not args.error:
+                        ax1.plot(frame2[xvar], frame2['latency_upper'], color='grey', alpha=0.4, linestyle='--')
+                        ax1.plot(frame2[xvar], frame2['latency_lower'], color='grey', alpha=0.4, linestyle='--')
+                        ax1.fill_between(frame2[xvar], frame2['latency_upper'], frame2['latency_lower'],
+                                         facecolor='grey', alpha=0.4, linestyle='--')
 
                 ax1.set_ylabel('Latency (ms)')
                 ax1.legend()
 
                 # second subplot with tp per item
                 ax2.plot(frame1[xvar], frame1[ycomparison.format(measure)], marker='+', label=f'{measure}/{xvar} {xlsfp.label[1]}', color='tab:red')
-                ax2.plot(frame1[xvar], frame1['tp_xvar_upper'], color='tab:red', label=f'{measure}/{xvar} error region',  alpha=0.4)
-                ax2.plot(frame1[xvar], frame1['tp_xvar_lower'], color='tab:red', alpha=0.4)
-                ax2.fill_between(frame1[xvar], frame1['tp_xvar_upper'], frame1['tp_xvar_lower'],
-                                facecolor='tab:red', alpha=0.4)
+                if not args.error:
+                    ax2.plot(frame1[xvar], frame1['tp_xvar_upper'], color='tab:red', label=f'{measure}/{xvar} error region',  alpha=0.4)
+                    ax2.plot(frame1[xvar], frame1['tp_xvar_lower'], color='tab:red', alpha=0.4)
+                    ax2.fill_between(frame1[xvar], frame1['tp_xvar_upper'], frame1['tp_xvar_lower'],
+                                    facecolor='tab:red', alpha=0.4)
                 if args.comparison:
                     ax2.plot(frame2[xvar], frame2[ycomparison.format(measure)], marker='x', label=f'{measure}/{xvar} {xlsfp2.label[1]}',
                              color='tab:red', linestyle='--')
-                    ax2.plot(frame2[xvar], frame2['tp_xvar_upper'], color='tab:red', alpha=0.4, linestyle='--')
-                    ax2.plot(frame2[xvar], frame2['tp_xvar_lower'], color='tab:red', alpha=0.4, linestyle='--')
-                    ax2.fill_between(frame2[xvar], frame2['tp_xvar_upper'], frame2['tp_xvar_lower'],
-                                     facecolor='tab:red', alpha=0.4)
+                    if not args.error:
+                        ax2.plot(frame2[xvar], frame2['tp_xvar_upper'], color='tab:red', alpha=0.4, linestyle='--')
+                        ax2.plot(frame2[xvar], frame2['tp_xvar_lower'], color='tab:red', alpha=0.4, linestyle='--')
+                        ax2.fill_between(frame2[xvar], frame2['tp_xvar_upper'], frame2['tp_xvar_lower'],
+                                         facecolor='tab:red', alpha=0.4)
 
                 ax2.set_xlabel(xlabel)
                 ax2.set_ylabel(f'Throughput ({unit})')
@@ -227,6 +233,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate graphs from spreadsheet of p11perftest results')
     parser.add_argument('xls', metavar='FILE', type=argparse.FileType('rb'), help='Path to Excel spreadsheet')
     parser.add_argument('-t', '--table', help='Table name', default='Sheet1')
+    parser.add_argument('-e', '--error', help='Remove error regions from plot', action='store_true')
 
     parser.add_argument('-c', '--comparison', help='Compare two datasets. Provide the path to a second Excel spreadsheet.', metavar='FILE', type=argparse.FileType('rb'))
 
